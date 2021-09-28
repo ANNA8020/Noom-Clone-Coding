36e4f2c5d119e807d0a56048accf9a50cd1a9160
ANNA8020 <86477995+ANNA8020@users.noreply.github.com>
2021-09-28 20:09:40 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+// http는 다른 protocol이라서 연결이 안 돼 → ws / wss
+// localhost:3000 보단 → window.location.host
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+    // JS object를 string으로 바꿔줌
+}
+
+function handleOpen() {
+    console.log("Connected to Server ✅");
+}
+
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
---
+++ server.js
@@ -0,0 +1,39 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+ const app = express();
+
+ app.set("view engine", "pug");
+ app.set("views", __dirname + "/views");
+ app.use("/public", express.static(__dirname + "/public"));
+//  public folder를 유저에게 공개해줌
+ app.get("/", (_,res) => res.render("home"));
+ app.get("/*", (_,res) => res.redirect("/"));
+//  catchell url
+ 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    // socket: 연결된 어떤 사람
+    sockets.push(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => {
+        // JSON.parse();
+        // string을 JS object로 바꿔줌
+        sockets.forEach((aSocket) => aSocket.send(message));
+        // 각 브라우저를 aSocket으로 표시하고 메시지를 보냄
+    });
+});
+
+server.listen(3000, handleListen);
